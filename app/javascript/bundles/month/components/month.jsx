c702b83b8682aade68591f26f7d3bec5d52c4f2a
Travis Crampton <crampton.travis@gmail.com>
2017-09-26 16:33:17 -0700

    all ready to go

--- app/javascript/bundles/month/components/month.jsx
+++ app/javascript/bundles/month/components/month.jsx
@@ -1,18 +1,22 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import Header from '../../flow/components/layouts/Header';
 
 export default class Month extends React.Component{
 	constructor(props, context){
 		super(props, context);
-
 		this.state = {}
 	}
 
 	render(){
 		return(
 			<div>
-				<h1>Month is ready to be rendered HERE </h1>
+				<Header bigText={this.props.date} subText='lets see how it goes'/>
 			</div>
 		)
 	}
+}
+
+Month.PropTypes = {
+
 }
